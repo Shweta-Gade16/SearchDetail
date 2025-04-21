20396ddca6f8d0450e201d0a3894fa5974bbd684
Shweta-Gade16 <shwetag@salttechno.com>
2025-04-21 14:03:11 +0530

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import MentorDetail from "./components/MentorDetail";
 
 export default function Home() {
   return (
-    <div className="w-full h-full bg-[#F4F4F4]">
+    <div className="w-full  h-full bg-[#F4F4F4]">
       <NavBar />
       <div className="w-full max-w-[1216px] px-4 sm:px-8 lg:px-16 mx-auto py-8">
         <Breadcrumb
@@ -13,7 +13,7 @@ export default function Home() {
             { label: "Melissa A." },
           ]}
         />
-        <div className="w-full h-auto p-4 sm:p-8 rounded-lg bg-white">
+        <div className="w-full max-w-[1216px] h-auto p-4 sm:p-8 rounded-lg bg-white">
           <MentorDetail />
         </div>
       </div>
